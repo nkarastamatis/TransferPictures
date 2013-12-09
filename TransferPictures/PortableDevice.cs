6843de0bd537887d48b7c1b3bd3a46f40640f470
Nick Karastamatis <nkarastamatis@gmail.com>
2013-12-09 10:05:45 -0500

    first

---
+++ TransferPictures/PortableDevice.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TransferPictures
+{
+    public abstract class PortableDeviceObject
+    {
+        protected PortableDeviceObject(string id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+    }
+    //Create a descendant of this abstract class to represent a folder.
+
+    public class PortableDeviceFolder : PortableDeviceObject
+    {
+        public PortableDeviceFolder(string id, string name)
+            : base(id, name)
+        {
+            this.Files = new List<PortableDeviceObject>();
+        }
+
+        public IList<PortableDeviceObject> Files { get; set; }
+    }
+    //Add another class to represent a file.
+
+    public class PortableDeviceFile : PortableDeviceObject
+    {
+        public PortableDeviceFile(string id, string name)
+            : base(id, name)
+        { }
+    }
+
+    public class PortableDeviceCollection : Collection<PortableDevice>
+    {
+        private readonly PortableDeviceManager _deviceManager;
+
+        public PortableDeviceCollection()
+        {
+            this._deviceManager = new PortableDeviceManager();
+        }
+
+        public void Refresh()
+        {
+            //...
+        }
+    }
+
+    public class PortableDevice 
+    {
+        public PortableDevice(string deviceId)
+        {
+            this.DeviceId = deviceId;
+        }
+
+        public string DeviceId { get; set; }
+        
+        public PortableDeviceFolder GetContents()
+        {
+            var root = new PortableDeviceFolder("DEVICE", "DEVICE");
+
+            IPortableDeviceContent content;
+            this._device.Content(out content);
+            EnumerateContents(ref content, root);
+
+            return root;
+        }
+    }
+}
---
+++ TransferPictures/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Management;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TransferPictures
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TransferPicturesForm());
+            var md = new MediaDrives();
+        }
+    }
+
+    public class MediaDrives
+    {
+        //public DriveInfo drives;
+
+        public MediaDrives()
+        {
+            var allDrives = DriveInfo.GetDrives();
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+                collection = searcher.Get();
+
+            
+            var drives = from drive in DriveInfo.GetDrives()
+                         where drive.DriveType == DriveType.Removable
+                         select drive;
+
+            foreach (var drive in drives)
+            {
+                Console.WriteLine(drive.Name);
+            }
+        }
+    }
+
+    private static PortableDeviceObject WrapObject(IPortableDeviceProperties properties, string objectId)
+        {
+            PortableDeviceApiLib.IPortableDeviceKeyCollection keys;
+            properties.GetSupportedProperties(objectId, out keys);
+
+            PortableDeviceApiLib.IPortableDeviceValues values;
+            properties.GetValues(objectId, keys, out values);
+
+            // Get the name of the object
+            string name;
+            var property = new PortableDeviceApiLib._tagpropertykey();
+            property.fmtid = new Guid(0xEF6B490D, 0x5CD8, 0x437A, 0xAF, 0xFC, 0xDA, 0x8B, 0x60, 0xEE, 0x4A, 0x3C);
+            property.pid = 4;
+            values.GetStringValue(property, out name);
+
+            // Get the type of the object
+            Guid contentType;
+            property = new PortableDeviceApiLib._tagpropertykey();
+            property.fmtid = new Guid(0xEF6B490D, 0x5CD8, 0x437A, 0xAF, 0xFC, 0xDA, 0x8B, 0x60, 0xEE, 0x4A, 0x3C);
+            property.pid = 7;
+            values.GetGuidValue(property, out contentType);
+
+            var folderType = new Guid(0x27E2E392, 0xA111, 0x48E0, 0xAB, 0x0C, 0xE1, 0x77, 0x05, 0xA0, 0x5F, 0x85);
+            var functionalType = new Guid(0x99ED0160, 0x17FF, 0x4C44, 0x9D, 0x98, 0x1D, 0x7A, 0x6F, 0x94, 0x19, 0x21);          
+
+            if (contentType == folderType || contentType == functionalType)
+            {
+                return new PortableDeviceFolder(objectId, name);
+            }
+
+            return new PortableDeviceFile(objectId, name);
+        }
+}
